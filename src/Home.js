c2a3e7414baacb0f44498e9bdccfdc8a956bef74
HaiderMonther <alardyhydr567890@gmail.com>
2023-09-04 13:51:56 +0300

    Ready to go

--- src/Home.js
+++ src/Home.js
@@ -7,9 +7,10 @@ function Home() {
         name: 'london',
         humidity:10,
         speed: 2,
-        image: ''
+        image: '/images/cloudy.png'
     })
     const [name, setName] = useState('');
+    const [error, setError] = useState('');
 
     const handleClick = ()=>{
         if(name !== "") {
@@ -17,11 +18,33 @@ function Home() {
             axios.get(apiUrl)
             .then(res => {
                 let imagePath = '';
-                
+                if(res.data.weather[0].main == "Clouds"){
+                    imagePath= "./images/clouds.png"
+                }else if(res.data.weather[0].main == "Clear"){
+                    imagePath="./images/sun.png";
+                }else if(res.data.weather[0].main == "Rain"){
+                    imagePath="./images/rain.png";
+                }else if(res.data.weather[0].main == "Drizzle"){
+                    imagePath="./images/drizzle.png";
+                }else if(res.data.weather[0].main == "Mist"){
+                    imagePath="./images/mist.png";
+                }else {
+                    imagePath= '/images/cloudy.png'
+                }
+                console.log(res.data);
                 setData({...data, celcius: res.data.main.temp, name: res.data.name,
-                     humidity: res.data.main.humidity, speed: res.data.wind.speed })
+                     humidity: res.data.main.humidity, speed: res.data.wind.speed, 
+                    image: imagePath})
+                    setError('');
             })
-            .catch(err =>console.log(err));
+            .catch(err => {
+                if (err.response.status == 404) {
+                    setError("Invalid City Name")
+                }else {
+                    setError('');
+                }
+                console.log(err)
+            });
         }
     }
   return (
@@ -31,9 +54,12 @@ function Home() {
                 <input type='text' placeholder='Enter City Name' onChange={e => setName(e.target.value)}/>
                 <button><img src='/images/loupe.png' alt='' onClick={handleClick}/></button>
             </div>
+            <div className='error'>
+                <p>{error}</p>
+            </div>
 
             <div className='winfo'>
-                <img src='/images/cloudy.png' className='icon'/>
+                <img src={data.image}className='icon'/>
                 <h1>{Math.round(data.celcius)}°C</h1>
                 <h2>{data.name}</h2>
                 <div className='details'>
